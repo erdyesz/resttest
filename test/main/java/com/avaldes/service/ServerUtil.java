9d9c9a44ead2f69afed43a06d90e197a235a21e6
erdogan.yesil <erdogan.yesil@t2.com.tr>
2017-12-06 16:37:09 +0300

    k

--- test/main/java/com/avaldes/service/ServerUtil.java
+++ test/main/java/com/avaldes/service/ServerUtil.java
@@ -12,20 +12,27 @@ import javax.ws.rs.core.UriBuilder;
 
 public class ServerUtil {
 	
-    public static final URI BASE_URI = UriBuilder.fromUri("http://localhost").port(8987).build();
+    public static final URI BASE_URI = UriBuilder.fromUri("http://localhost").port(9765).build();
 	
 	/**
 	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
 	 * @return Grizzly HTTP server.
 	 */
 	public static HttpServer startServer(String packagePath) {
-	    // create a resource config that scans for JAX-RS resources and providers
-	    // in $package package
-	    final ResourceConfig rc = new ResourceConfig().packages(packagePath);
+	    try {
+			// create a resource config that scans for JAX-RS resources and providers
+			// in $package package
+			final ResourceConfig rc = new ResourceConfig().packages(packagePath);
 
-	    // create and start a new instance of grizzly http server
-	    // exposing the Jersey application at BASE_URI
-	    return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+			// create and start a new instance of grizzly http server
+			// exposing the Jersey application at BASE_URI
+			return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(BASE_URI);
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 }
