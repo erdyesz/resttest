d11987afc2ae5e8c5048e932f8e460e9695e5855
erdogan.yesil <erdogan.yesil@t2.com.tr>
2017-12-12 23:18:36 +0300

    test echo

--- bin/docker_deploy.sh
+++ bin/docker_deploy.sh
@@ -7,6 +7,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     pwd
     ls
     #./bin/ecs_deploy.sh -c $CLUSTER_NAME -n $SERVICE -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH
+    echo "eval $(aws ecs update-service --region ${AWS_DEFAULT_REGION} --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME}  --task-definition ${TASK_DEFINITION})"
     eval $(aws ecs update-service --region "${AWS_DEFAULT_REGION}" --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}"  --task-definition "${TASK_DEFINITION}")
   else
     echo "Skipping deploy because it's not an allowed branch"
